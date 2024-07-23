f6b9187bc229faf9fe2ad2c0f099a306d2900221
DanielOchoaSuarez <d.ochoas@uniandes.edu.co>
2024-07-23 12:17:35 -0500

    Avance

--- src/app/pages/add-owner/add-owner.component.ts
+++ src/app/pages/add-owner/add-owner.component.ts
@@ -1,27 +1,10 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-owner',
   templateUrl: './add-owner.component.html',
-  styleUrls: ['./add-owner.component.css'],
+  styleUrls: ['./add-owner.component.css']
 })
 export class AddOwnerComponent {
-  ownerForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {
-    this.ownerForm = this.fb.group({
-      firstName: ['', Validators.required],
-      lastName: ['', Validators.required],
-      address: [''],
-      city: [''],
-      telephone: [''],
-    });
-  }
-
-  onSubmit() {
-    if (this.ownerForm.valid) {
-      console.log('Form Value:', this.ownerForm.value);
-    }
-  }
 }
--- src/app/pages/find-owner/find-owner.component.ts
+++ src/app/pages/find-owner/find-owner.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -6,18 +6,29 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   templateUrl: './find-owner.component.html',
   styleUrls: ['./find-owner.component.css'],
 })
-export class FindOwnerComponent {
+export class FindOwnerComponent implements OnInit {
   findOwnerForm: FormGroup;
 
   constructor(private fb: FormBuilder) {
     this.findOwnerForm = this.fb.group({
-      lastName: ['', [Validators.required]],
+      lastName: ['', Validators.required],
     });
   }
 
-  onSubmit() {
+  ngOnInit(): void {}
+
+  onSubmit(): void {
     if (this.findOwnerForm.valid) {
-      console.log('Form Value:', this.findOwnerForm.value);
+      console.log('Form Submitted!', this.findOwnerForm.value);
+    } else {
+      console.log('Form not valid');
     }
+  }
+
+  getErrorMsg(field: any): string {
+    if (field.errors.required) {
+      return 'Last name is required';
+    }
+    return '';
   }
 }
